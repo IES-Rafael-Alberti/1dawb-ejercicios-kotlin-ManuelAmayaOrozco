008ead7633ff46ffb5699a9fb3bf6b2df80e6a16
mamaoro1405 <mamaoro1405@g.educaand.es>
2023-12-18 19:39:17 +0100

    Actualización Ejercicios Kotlin

--- EjerciciosKotlin/src/main/kotlin/EjercicioU2_Condicionales.kt
+++ EjerciciosKotlin/src/main/kotlin/EjercicioU2_Condicionales.kt
@@ -15,3 +15,26 @@ fun tPassword(password: String): Boolean {
     val passOrig = "contraseña"
     return password.replace(" ", "") == passOrig
 }
+
+fun ejercicio2_cond_3() {
+    print("Dime un número: ")
+    val num1 = readln().toInt()
+    print("Dime otro número: ")
+    val num2 = readln().toInt()
+    val res = division(num1, num2)
+    if (res == -1) {
+        print("Error, no se puede dividir por 0.")
+    }
+    else {
+        print(res)
+    }
+}
+
+fun division(num1: Int, num2: Int) =
+    if (num2 == 0) {
+        -1
+    }
+    else {
+        val res = num1.toDouble() / num2
+        res
+    }
